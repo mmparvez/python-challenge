fc880e836a6727d9c08baa38b2b76259191494a1
Parvez <alampmail@gmail.com>
2022-08-21 14:59:33 +1000

    2nd commit: More comments in the code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,4 @@
-# This will allow us to create file paths across operating systems
+# create file paths across operating systems
 import os
 
 # Module for reading CSV files
@@ -8,46 +8,49 @@ import csv
 # Set working directory to where this Python file is
 os.chdir(os.path.dirname(__file__))
 
-# Optional: print the current working directory
-print("This program is running from: " + os.getcwd())
-#-----------------------------------------------------
+#Set path for file
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
+#Define function for average change in profit/loss
 def Average(lst):
     return sum(lst) / len(lst)
-# Method 2: Improved Reading using CSV module
+
+#Defining variables and lists for calculations
+month_count = 0
+ttl_value = 0
+month=[]
+value=[]
+changes=[]
+
+#Reading using CSV module
 
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    #print(csvreader)
-
-    # Read the header row first (skip this step if there is now header)
+    # Read the header row
     csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
 
-    month_count = 0
-    ttl_value = 0
-    month=[]
-    value=[]
-    changes=[]
-
-        # Read each row of data after the header
+    # Read each row of data after the header
     for row in csvreader:
-        month_count = month_count + 1
-        month.append(row[0])
-        ttl_value = ttl_value + int(row[1])
-        value.append(int(row[1]))
+        month.append(row[0]) #add each month to month list
+        value.append(int(row[1])) #add each value to value list
     
-    for i in range(1,len(value)):
-        changes.append(value[i] - value[i-1])
-    Average_change = Average(changes)
-    Average_change = round(Average_change,2)
-    Max_change = max(changes)
-    Min_change = min(changes)
-    Max_month = month[changes.index(Max_change) + 1]
+    month_count = len(month) #set month_count to length of month list
+    ttl_value = sum(value) #set ttl_value to sum of value list
+
+    #loop through value list
+    for i in range(1,len(value)): 
+        changes.append(value[i] - value[i-1]) #add each change to changes list
+    
+    Average_change = Average(changes) #calling average function to set Average_change
+    Average_change = round(Average_change,2) #round to 2 decimal places
+    Max_change = max(changes) #set Max_change to max of changes list
+    Min_change = min(changes) #set Min_change to min of changes list
+    #set month for the max and min change
+    #first month's changes on profit/loss was 0, so add 1 to index
+    Max_month = month[changes.index(Max_change) + 1] 
     Min_month = month[changes.index(Min_change) + 1]
 
 # Specify the file to write to
@@ -55,7 +58,7 @@ output_path = os.path.join("Analysis", "Result.txt")
 
 # Open the file using "write" mode. Specify the variable to hold the contents
 with open(output_path, 'w') as txtfile:
-
+    # Writing in the file
     filewriter = txtfile.write("Financial Analysis\n")
     filewriter = txtfile.write("----------------------------\n")
     filewriter = txtfile.write(f"Total Months: {month_count}\n")
@@ -64,7 +67,7 @@ with open(output_path, 'w') as txtfile:
     filewriter = txtfile.write(f"Greatest Increase in Profits: {Max_month} (${Max_change})\n")
     filewriter = txtfile.write(f"Greatest Decrease in Profits: {Min_month} (${Min_change})\n")
 
-
+# Printing in the terminal
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {month_count}")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,68 +1,62 @@
-# This will allow us to create file paths across operating systems
+# create file paths across operating systems
 import os
 
 # Module for reading CSV files
 import csv
 
-#-----------------------------------------------------
-# Set working directory to where this Python file is
 os.chdir(os.path.dirname(__file__))
 
-# Optional: print the current working directory
-print("This program is running from: " + os.getcwd())
-#-----------------------------------------------------
+#Setting working directory to where this Python file is
 csvpath = os.path.join('Resources', 'election_data.csv')
 
-def Average(lst):
-    return sum(lst) / len(lst)
-# Method 2: Improved Reading using CSV module
+#Define variables and dictionary{candidate: Vote count} for calculations
+vote_count = 0
+candidate={}
 
+#Reading using CSV module
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    #print(csvreader)
-
-    # Read the header row first (skip this step if there is now header)
+    # Read the header row
     csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
 
-    vote_count = 0
-    candidate={}
-    
-
-
-        # Read each row of data after the header
+    # Read each row of data after the header
     for row in csvreader:
-        vote_count = vote_count + 1
-        if row[2] in candidate:
-            candidate[row[2]] = candidate[row[2]] + 1
-        else:
-            candidate[row[2]] = 1
+        vote_count = vote_count + 1 #add 1 to vote_count for each row
+
+          
+        if row[2] in candidate: #if candidate is in candidate dictionary add 1 to vote count
+            candidate[row[2]] = candidate[row[2]] + 1 
+        
+        else: #if candidate is not in dictionary, add candidate and 1 to vote count 
+            candidate[row[2]] = 1 
    
 # Specify the file to write to
 output_path = os.path.join("Analysis", "Result.txt")
 
 # Open the file using "write" mode. Specify the variable to hold the contents
 with open(output_path, 'w') as txtfile:
-
+#wrining the header and total votes in the file
     filewriter = txtfile.write("Election Results\n")
     filewriter = txtfile.write("----------------------------\n")
     filewriter = txtfile.write(f"Total votes: {vote_count}\n")
     filewriter = txtfile.write("----------------------------\n")
-
+#printing the header and total votes in the terminal
     print("Election Results")
     print("----------------------------")
     print(f"Total votes: {vote_count}")
     print("----------------------------")
-
+#wrinting/printing the candidate and vote count from candidate dictionary
     for key in candidate:
         vote_percentage = (candidate[key]/vote_count)*100
         filewriter = txtfile.write(f"{key}: {vote_percentage:.3f}% ({candidate[key]})\n")
         print(f"{key}: {vote_percentage:.3f}% ({candidate[key]})")
-    
+#find and return the candidate with the most votes    
     winner = max(candidate, key=candidate.get)
+
+#wrinting/printing the winner
     filewriter = txtfile.write("----------------------------\n")
     filewriter = txtfile.write(f"Winner: {winner}\n")
     filewriter = txtfile.write("----------------------------\n")
